0533e25169dccdeb3bae7a65db820fee1fad8187
Frank Porto <55335409+frank049@users.noreply.github.com>
2020-04-13 01:42:56 -0400

    Version 2.0

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: Bien Frank 16 - tu primer paso en CICD");
+  res.status('200').send("Status: Bien Frank 16 - tu primer paso en giCICD");
 });
 
 // retrieve all musicians from data store
